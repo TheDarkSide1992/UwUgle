6a62282027a5fe86d132dc620661dfc931aa0514
emil476m <emilwiltmann.dk@gmail.com>
2025-03-06 14:30:35 +0100

    Created project for FileIndexer Service

---
+++ Apps/FileIndexer/Infrastructure/IndexRepository.cs
@@ -0,0 +1,13 @@
+﻿using Npgsql;
+
+namespace Infrastructure;
+
+public class IndexRepository
+{
+    private readonly NpgsqlDataSource _dataSource;
+
+    public IndexRepository(NpgsqlDataSource dataSource)
+    {
+        _dataSource = dataSource;
+    }
+}
