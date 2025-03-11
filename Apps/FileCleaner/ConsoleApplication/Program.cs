f070f8ddbece1fb9fd33ded9b4f77c182752d16b
TheDarkSide1992 <Jenscp171@gmail.com>
2025-03-11 12:43:24 +0100

    init for header remowal can remowe some part of a mail header string

--- Apps/FileCleaner/ConsoleApplication/Program.cs
+++ Apps/FileCleaner/ConsoleApplication/Program.cs
@@ -1,3 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using Service;
+using Service.Implementations;
+
 Console.WriteLine("Hello, World!");
+
+CleanerService cs = new CleanerService(new CleanerString());
+cs.start();
+
---
+++ Apps/FileCleaner/Service/Interfaces/ICleaner.cs
@@ -0,0 +1,6 @@
+﻿namespace Service.Interfaces;
+
+public interface ICleaner<T> where T : class
+{
+    Task<T> Clean(T message);
+}
