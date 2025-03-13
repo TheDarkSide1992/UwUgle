02fbb004a14f2ebea09512a87500a987d48086e9
andreas14101 <andreasshansen@live.dk>
2025-03-13 18:59:40 +0100

    set up circuit breaker

--- Apps/FileIndexer/ConsoleApplication/Program.cs
+++ Apps/FileIndexer/ConsoleApplication/Program.cs
@@ -1,19 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System.Text;
+using EasyNetQ;
+using Events;
 using Logger;
+using Polly;
+using Polly.CircuitBreaker;
+using Serilog;
 using Service;
 
 public class Program
 {
     public static async Task Main()
     {
-        var connectionEstablished = false;
-        Thread.Sleep(5000);
-        using var bus = ConnectionHelper.GetRabbitMQConnection();
-        while (!connectionEstablished)
+        AsyncCircuitBreakerPolicy policy = Policy.Handle<Exception>().CircuitBreakerAsync(3, TimeSpan.FromSeconds(60),
+            onBreak: (exception, TimeSpan) =>
+            {
+                Log.Logger.Warning("Circuit breaker in indexer is on brake");
+            }, onReset: () => {Log.Logger.Information("Circuit breaker in indexer is reset");},
+            onHalfOpen: () => {Log.Logger.Information("Circuit breaker in indexer is half open");});
+        policy.ExecuteAsync(async () =>
         {
-            
-        }
+
+        });
     }
 }
