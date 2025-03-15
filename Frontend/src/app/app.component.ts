2d74dd445cb35697dd4aa8eff2d528bcb7847d7e
TheDarkSide1992 <Jenscp171@gmail.com>
2025-03-15 12:00:15 +0100

    added search bar

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,13 +1,54 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {DocumentSearchResult} from "./models/DocumentSearchResult.model";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {environment} from "./Envirements/envirement";
+import {FormsModule} from "@angular/forms";
+import {NgForOf, NgIf} from "@angular/common";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgForOf, NgIf],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'Frontend';
+  query: string = '';
+  documentSearchResults: DocumentSearchResult[] = [];
+
+  constructor(private http: HttpClient) {
+  }
+
+  async searchDocumentsTest(query: string) {
+    let txty : DocumentSearchResult = {
+      DocumentID : "this.q",
+      DocumentName : query,
+    }
+    this.documentSearchResults.push(txty)
+
+    console.log(query);
+  }
+
+  async getDocuement(DocumentID: string){
+    console.log(DocumentID);
+  }
+
+  async searchDocuments(query: string) {
+    try {
+      const headers = new HttpHeaders({"Access-Control-Allow-Origin": "*", "Content-Type": "application/json", "Access-Control-Allow-Methods": "GET"});
+
+      const call = this.http.get<DocumentSearchResult[]>(environment.baseURL + "query=" + query, {headers: headers});
+      this.documentSearchResults = [];
+      call.subscribe((resData: DocumentSearchResult[]) => {
+        this.documentSearchResults.push(...resData);
+      })
+    } catch (error) {
+    }
+
+    console.log(this.documentSearchResults);
+
+  }
+
 }
