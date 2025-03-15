ee8aa2e6520b7ada556368bb2a7ef4f0a286dd8d
andreas14101 <andreasshansen@live.dk>
2025-03-15 10:24:26 +0100

    added circuitbraker for fault isonation and set up docker-compose and tested if service kept running

--- Apps/FileIndexer/ConsoleApplication/Program.cs
+++ Apps/FileIndexer/ConsoleApplication/Program.cs
@@ -1,9 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System.Text;
+﻿using System.Diagnostics;
 using EasyNetQ;
 using Events;
+using Events.EventModels;
 using Logger;
+using OpenTelemetry;
+using OpenTelemetry.Context.Propagation;
 using Polly;
 using Polly.CircuitBreaker;
 using Serilog;
@@ -11,17 +12,80 @@ using Service;
 
 public class Program
 {
+   private static AsyncCircuitBreakerPolicy policy = Policy.Handle<Exception>().CircuitBreakerAsync(3, TimeSpan.FromSeconds(30),
+        onBreak: (exception, TimeSpan) =>
+        {
+            using var activity = Monitoring.ActivitySource.StartActivity();
+            Log.Logger.Warning("Circuit breaker in Indexer brake");
+        },  onReset: async () =>
+        {
+            using var activity = Monitoring.ActivitySource.StartActivity();
+            Log.Logger.Information("Circuit breaker in Indexer is reset");
+            await Main();
+        },
+        onHalfOpen: () =>
+        {
+            using var activity = Monitoring.ActivitySource.StartActivity();
+            Log.Logger.Information("Circuit breaker in Indexer is half open");
+        });
+    
     public static async Task Main()
     {
-        AsyncCircuitBreakerPolicy policy = Policy.Handle<Exception>().CircuitBreakerAsync(3, TimeSpan.FromSeconds(60),
-            onBreak: (exception, TimeSpan) =>
-            {
-                Log.Logger.Warning("Circuit breaker in indexer is on brake");
-            }, onReset: () => {Log.Logger.Information("Circuit breaker in indexer is reset");},
-            onHalfOpen: () => {Log.Logger.Information("Circuit breaker in indexer is half open");});
-        policy.ExecuteAsync(async () =>
+        while (true)
         {
+            await handleMessages();
+            Thread.Sleep(1000);
+        }
+    }
 
-        });
+
+    public static async Task handleMessages()
+    {
+        var service = new IndexService();
+        var connectionEstablished = false;
+        using var bus = RabbitMqConnectionHelper.GetRabbitMQConnection();
+        while (!connectionEstablished)
+        {
+            policy.ExecuteAsync(async () =>
+            {
+                {
+                    var subscriptionResult = bus.PubSub.SubscribeAsync<CleanedEvent>("Cleaned", async e =>
+                    {
+                        var propagator = new TraceContextPropagator();
+                        var parentContext = propagator.Extract(default, e, (msg, key) =>
+                        {
+                            return new List<string>(new[]
+                                { msg.Headers.ContainsKey(key) ? msg.Headers[key].ToString() : string.Empty });
+                        });
+                        Baggage.Current = parentContext.Baggage;
+                        using var activity = Monitoring.ActivitySource.StartActivity("Message Received",
+                            ActivityKind.Consumer, parentContext.ActivityContext);
+                        
+                        using var indexActivity = Monitoring.ActivitySource.StartActivity();
+                        
+                        var indexStatus = await service.Index(e.CleanMessage);
+                        if (indexStatus)
+                        {
+                          Log.Logger.Information("Index of file completed");   
+                        }
+                        else
+                        {
+                            Log.Logger.Error("Index of file failed");
+                        }
+                        
+                    }).AsTask();
+
+                    await subscriptionResult.WaitAsync(CancellationToken.None);
+                    connectionEstablished = subscriptionResult.Status == TaskStatus.RanToCompletion;
+                    if (!connectionEstablished) Thread.Sleep(1000);
+                }
+                var wait = true;
+                while (wait)
+                {
+                    Thread.Sleep(1000);
+                    wait = false;
+                }
+            });
+        }
     }
 }
